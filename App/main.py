723352d8ccd7c5704181790a172b43567c3944e4
Dmytro Frolov <dmfroloff@gmail.com>
2021-11-10 13:06:44 +0200

    convert ApiHandler to static

--- App/main.py
+++ App/main.py
@@ -5,20 +5,18 @@ from LobbyAnalyzer.LobbyAnalyzer import LobbyAnalyzer
 
 
 if __name__ == '__main__':
-    print()
-    hnd = ApiHandler()
     # TODO: cache session
-    session_id = hnd.create_session()
+    session_id = ApiHandler.create_session()
     print(f'session: {session_id}')
 
     # TODO: autofetch lobby data for development
-    # mid = hnd.custom_request(method_name='getmatchidsbyqueue', session=session_id,
+    # mid = ApiHandler.custom_request(method_name='getmatchidsbyqueue', session=session_id,
                             #  custom_params=f'/{Queue.Conquest}/{cuts.make_date()}/18,30')[0]['Match']
-    mid = hnd.get_match_id(session_id)
+    mid = ApiHandler.get_match_id(session_id)
     if mid != 0:
-        match_details = hnd.get_match_details(session_id, mid)
+        match_details = ApiHandler.get_match_details(session_id, mid)
         analyzer = LobbyAnalyzer(match_details)
-        analyzer.analyze_player_background(hnd.get_player_batch(session_id, analyzer.get_player_id_list()))
+        analyzer.analyze_player_background(ApiHandler.get_player_batch(session_id, analyzer.get_player_id_list()))
         analyzer.print_lobby_stats()
     else:
         print("Match has not started yet!")
