964f5eee10bb7c865a62e2fed798409ca973c54e
Dmytro Frolov <dmfroloff@gmail.com>
2021-11-13 22:34:03 +0200

    add automatic session refresh after expiration

--- App/ApiHandling/Session.py
+++ App/ApiHandling/Session.py
@@ -1,4 +1,5 @@
 import time
+import threading
 import sys
 
 from ApiHandling.ApiHandler import ApiHandler
@@ -6,50 +7,104 @@ import Common.Utils as cuts
 
 
 class Session:
-    SESSION_LIFETIME_LIMIT = 15*60 # 15minutes is a session lifetime limited by HiRez API
-    SESSION_CACHE_LOCATION = 'tmp/cache_session_data.json'
-
+    """
+    Container-class for session
+    """
+    LIFETIME_LIMIT = 15*60 # 15minutes is a session lifetime limited by HiRez API
+    CACHE_LOCATION = 'tmp/cache_session_data.json'
     def __init__(self):
-        self.load_cached_session()
+        self.id = None
+        self.creation_time = None
 
-        if not self.is_session_valid():
+    def update_with_sid(self, sid):
+        self.id = sid
+        self.creation_time = time.time()
+
+class SessionManager:
+    """
+    Class resposible for keeping the session fresh and juicy
+    """
+    def __init__(self):
+        self._session = Session()
+        # blocks __str__ method call (called when someone wants to obtain session id) in case session is reloading
+        self.session_reload_lock = threading.Lock()
+
+        self.load_cached_session()
+        if not SessionManager.is_session_valid(self._session):
             # create a new session
-            self.refresh_session()
+            self.update_session(self._session)
             # and cache it
-            self.cache_session()
+            SessionManager.cache_session(self._session)
+        self.create_session_refresh_timer()
 
     def __str__(self) -> str:
-        return self.session_id
+        with self.session_reload_lock:
+            return self._session.id
 
-    def is_session_valid(self) -> bool:
-        """Checks if the session could be considered as valid, handles scenarios when session was or was not loaded
-            from cache
-
-        Session is considered valid only if we successfully loaded it and its lifetime is still valid
+    @staticmethod
+    def is_session_valid(session) -> bool:
+        """Checks if the session is valid.
+        Session is considered valid only if it was successfully loaded it and its lifetime has not expired yet
 
         Returns:
             bool: Whether or not session could be considered as a valid one
         """
         print("Validating session...")
         session_valid = False
-        # self.session_creation_time will be created in case session was loaded successfully from cache
-        if hasattr(self, "session_creation_time"):
-            # {current time} - {session create time} < {SESSION_LIFETIME_LIMIT}
-            session_valid = time.time() - self.session_creation_time < Session.SESSION_LIFETIME_LIMIT
-        print(f"Session is {'not ' if not session_valid else ''}valid.")
+        if session.creation_time is not None:
+            # check if session lifetime has not expired
+            # {current time} - {session create time} < {LIFETIME_LIMIT}
+            session_valid = time.time() - session.creation_time < Session.LIFETIME_LIMIT
+        print(f"Session is {'' if session_valid else 'not '}valid.")
         return session_valid
 
+    def update_session(self, session) -> None:
+        with self.session_reload_lock:
+            session.update_with_sid(ApiHandler.create_session()['session_id'])
+
+######
+# Context management functionality
+######
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if hasattr(self, 'timer'):
+            self.timer.cancel()
+
+######
+# Session refresh functionality
+######
+    def create_session_refresh_timer(self):
+        # session should be refreshed 5s before its expiration time
+        backup_time = 5
+        planned_session_refresh_time = self._session.creation_time + Session.LIFETIME_LIMIT - backup_time
+        refresh_after = max([0, planned_session_refresh_time - time.time()])
+        print(f"Session will be refreshed after {int(refresh_after)}sec")
+        self.timer = threading.Timer(interval=refresh_after, function=self._timer_callback)
+        self.timer.start()
+
+    def _timer_callback(self):
+        # create a new session
+        self.update_session(self._session)
+        # and cache it
+        SessionManager.cache_session(self._session)
+        # set new timer
+        self.create_session_refresh_timer()
+
+######
+# Session cache functionality
+######
     def load_cached_session(self) -> None:
         try:
-            self.__dict__ = cuts.json_contents(Session.SESSION_CACHE_LOCATION)
-            print(f"Data was successfully loaded from cache: {self.__dict__}")
+            with self.session_reload_lock:
+                # update instead of assign to avoid accidental data erase
+                self._session.__dict__.update(cuts.json_contents(Session.CACHE_LOCATION))
+                print(f"Data was successfully loaded from cache: {self._session.__dict__}")
         except:
-            print("No cached session found.")
+            print(f"No cached session found")
 
-    def cache_session(self) -> None:
-        print(f"Session cached to {Session.SESSION_CACHE_LOCATION} as {self.__dict__}")
-        cuts.dump_to_json(Session.SESSION_CACHE_LOCATION, self.__dict__)
-
-    def refresh_session(self) -> None:
-        self.session_creation_time = time.time()
-        self.session_id = ApiHandler.create_session()['session_id']
+    @staticmethod
+    def cache_session(session) -> None:
+        print(f"Session cached to {Session.CACHE_LOCATION} as {session.__dict__}")
+        cuts.dump_to_json(Session.CACHE_LOCATION, session.__dict__)
--- App/main.py
+++ App/main.py
@@ -1,20 +1,22 @@
 import Common.Utils as cuts
-from ApiHandling.Session import Session
+from ApiHandling.Session import SessionManager
 from ApiHandling.ApiHandler import ApiHandler
 from ApiHandling.QueueIds import Queue
 from LobbyAnalyzer.LobbyAnalyzer import LobbyAnalyzer
 
 
 if __name__ == '__main__':
-    session_id = Session()
-    # TODO: autofetch lobby data for development
-    # mid = ApiHandler.custom_request(method_name='getmatchidsbyqueue', session=session_id,
-                            #  custom_params=f'/{Queue.Conquest}/{cuts.make_date()}/18,30')[0]['Match']
-    mid = ApiHandler.get_match_id(session_id)
-    if mid != 0:
-        match_details = ApiHandler.get_match_details(session_id, mid)
-        analyzer = LobbyAnalyzer(match_details)
-        analyzer.analyze_player_background(ApiHandler.get_player_batch(session_id, analyzer.get_player_id_list()))
-        analyzer.print_lobby_stats()
-    else:
-        print("Match has not started yet!")
+    with SessionManager() as session_inst:
+        # TODO: autofetch lobby data for development
+        mid = ApiHandler.get_match_id(session_inst)
+        if mid == 0: # if not in lobby
+            print("Loading random match data for test sake")
+            mid = ApiHandler.common_request(method_name='getmatchidsbyqueue', session=session_inst,
+                                                 custom_params=f'/{Queue.Conquest}/{cuts.make_date()}/19,50')[0]['Match']
+        if mid != 0:
+            match_details = ApiHandler.get_match_details(session_inst, mid)
+            analyzer = LobbyAnalyzer(match_details)
+            analyzer.analyze_player_background(ApiHandler.get_player_batch(session_inst, analyzer.get_player_id_list()))
+            analyzer.print_lobby_stats()
+        else:
+            print("Match has not started yet!")
